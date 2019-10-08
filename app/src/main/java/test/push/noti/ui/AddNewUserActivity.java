eb2cf83984f217fcc0fbf58448f08f826cb7d743
BEAN <sbean0215@gmail.com>
2019-10-09 01:25:08 +0900

    add basic Activity classes and layouts (entrance, AddNewUser)

---
+++ app/src/main/java/test/push/noti/ui/AddNewUserActivity.java
@@ -0,0 +1,17 @@
+package test.push.noti.ui;
+
+import test.push.noti.R;
+import test.push.noti.base.BaseActivity;
+
+public class AddNewUserActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_add_new_user;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
---
+++ app/src/main/java/test/push/noti/ui/EntranceActivity.java
@@ -0,0 +1,17 @@
+package test.push.noti.ui;
+
+import test.push.noti.R;
+import test.push.noti.base.BaseActivity;
+
+public class EntranceActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_entrance;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
