e4256f002633373137f0fd359b5006f893b229ed
BEAN <sbean0215@gmail.com>
2019-10-16 22:01:05 +0900

    데이터 바인딩 추가, onClick 메서드 연결 (Entrance, AddNewUser Activities)

--- app/src/main/java/test/push/noti/ui/AddNewUserActivity.java
+++ app/src/main/java/test/push/noti/ui/AddNewUserActivity.java
@@ -1,9 +1,14 @@
 package test.push.noti.ui;
 
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import test.push.noti.BR;
 import test.push.noti.R;
 import test.push.noti.base.BaseActivity;
 
 public class AddNewUserActivity extends BaseActivity {
+
+    ViewDataBinding binding;
 
     @Override
     public int getLayoutId() {
@@ -13,5 +18,12 @@ public class AddNewUserActivity extends BaseActivity {
     @Override
     public void initView() {
 
+        binding = DataBindingUtil.setContentView(this, getLayoutId());
+        binding.setVariable(BR.activity, this);
+
+    }
+
+    public void closeActivity() {
+        finish();
     }
 }
--- app/src/main/java/test/push/noti/ui/EntranceActivity.java
+++ app/src/main/java/test/push/noti/ui/EntranceActivity.java
@@ -1,9 +1,16 @@
 package test.push.noti.ui;
 
+import android.content.Intent;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import test.push.noti.BR;
 import test.push.noti.R;
 import test.push.noti.base.BaseActivity;
 
 public class EntranceActivity extends BaseActivity {
+
+    ViewDataBinding binding;
+
 
     @Override
     public int getLayoutId() {
@@ -12,6 +19,12 @@ public class EntranceActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        binding = DataBindingUtil.setContentView(this, getLayoutId());
+        binding.setVariable(BR.activity, this);
 
+    }
+
+    public void openAddNewUserActivity() {
+        startActivity(new Intent(this, AddNewUserActivity.class));
     }
 }
